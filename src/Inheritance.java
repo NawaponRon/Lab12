8c7f25b75f414ea5e9ddc3567d134da3522cab6c
Lab <Lab@10.9.26.143>
2016-11-17 10:30:51 +0700

    การวืบทอด คาส

--- src/Inheritance.java
+++ src/Inheritance.java
@@ -1,58 +1,82 @@
-
 public class Inheritance {
 
 	public static void main(String[] args) {
-	
 		Ant ant = new Ant();
 		Cat cat = new Cat();
-		ant.walk();
-		cat.walk();
-		cat.eat();
-		ant.eat();
-
+				
+		System.out.println("Ant is "+ant.getType());	
+		System.out.println("Cat is "+cat.getType());
+		
 	}
 
 }
-abstract class Activity{
+interface Activity {
 	abstract void walk();
 	abstract void run();
-	abstract void eat(); 
-	
+	abstract void eat();
 }
+class Animal {
+	String type;
 
-class Ant extends Activity {
-	@Override
-	void walk(){
-		System.out.println("Ant walks on the floor");
+	public Animal() {
+		super();
+		this.type = "Unknown";
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
 	}
 	
+}
+class Ant extends Animal implements Activity {
+
+	public Ant() {
+		super();		
+		setType("Insect");
+	}
+
 	@Override
-	void run(){
+	public void walk() {
+		System.out.println("Ant walks on the floor");		
+	}
+
+	@Override
+	public void run() {
 		System.out.println("Ant runs .......");
+		
 	}
-	
-	@Override
-	void eat(){
-		System.out.println("Ant eat sugar");
-	}
-	
-}
-class Cat extends Activity{
-	@Override
-	void walk(){
-		System.out.println("Cat walks on the floor");
-	}
-	
-	@Override
-	void run(){
-		System.out.println("Cat runs .......");
-	}
-	
-	@Override
-	void eat(){
-		System.out.println("Cat eat fish");
-	}
-	
-}
-	
 
+	@Override
+	public void eat() {
+		System.out.println("Ant eats sugar");
+		
+	}
+	
+}
+class Cat extends Animal implements Activity {
+
+	public Cat() {
+		super();
+		setType("Carnivorous");
+	}
+
+	@Override
+	public void walk() {
+		System.out.println("Cat walks on the floor");		
+	}
+
+	@Override
+	public void run() {
+		System.out.println("Cat runs .......");		
+	}
+
+	@Override
+	public void eat() {
+		System.out.println("Cat eats fish");		
+	}
+	
+}
